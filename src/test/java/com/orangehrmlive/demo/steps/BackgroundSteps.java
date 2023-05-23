eebc0730f4203e1161cb3e19b4d0c8650c9e0686
Sanketpatel30 <snkt301987@gmail.com>
2023-05-23 21:31:57 +0100

    HW

---
+++ src/test/java/com/orangehrmlive/demo/steps/BackgroundSteps.java
@@ -0,0 +1,12 @@
+package com.orangehrmlive.demo.steps;
+
+import cucumber.api.java.en.Given;
+
+
+public class BackgroundSteps {
+
+    @Given("^I am Login Page$")
+    public void iAmLoginPage() {
+    }
+
+}
